34f8a2f8724c6757a8625e009a9508bf08b38d1d
Battu Gopi Krishna <gopikrishnabattu7@gmail.com>
2025-10-23 23:38:09 +0530

    python Note updated upto keywords and datatype

---
+++ PythonNotes.py
@@ -0,0 +1,85 @@
+"""Hello everyone!
+This is my personal Python Notes where I can use for my future refer and also to available every thing
+ in One Place"""
+"""
+In the below code whatever having hashcode '#' you can remove that part of code will execute
+if you wish to run multipule line of code and you need remove lot of '/' then there is a tip to do in a single click
+by selecting the required code just press 'cltr + / (in windows)' 'command + /(in MacOS)'
+"""
+# Python is high level programming language because this can be understood by users and the computer also
+# And Programming language is a medium which helps us to communicate with system
+
+"""
+Python Literals
+
+literals are nothing but numbers
+Integers and Floats 
+Integer = 1, 2, 3, 100, 1000, 0, -1, -2, -3, -100, -1000
+Floats = 1.0, 2.23, 3.78, 100.00, 0.00, -1.0, -2.456, -1000.423
+Below are the Examples
+"""
+# a = 10
+# print(a)
+# print(type(a))
+# aa = 10.0
+# print(aa)
+# print(type(aa))
+
+"""
+Python Variable
+The variable will reserve memory location.
+example a = 10 that means we are assign the 10 to the variable 'a'
+Note: Python is a case sensitive example: a = 10 and print(A) the output will be error and if you try print(a) then the output is 10.
+"""
+
+"""
+If you want to comment in the python code 
+For single line comment use - ‘#’
+For the multiple line comment use - ‘ “ “ “ ‘ at the starting of the line and ‘ “ “ “ ‘ and at the ending of the comment line.
+Python Datatype
+Primitive Datatypes
+-Integers
+-Floats
+-Strings
+-Boolean
+-None (spl datatype)
+"""
+# #Example for the Integer
+# aage = 25
+# print(aage)
+# print(type(aage))
+# #Example for the Float
+# percentt = 89.90
+# print(percentt)
+# print(type(percentt))
+# #Example for the String
+# peru = 'GOPI'
+# print(peru)
+# print(type(peru))
+# # here we have number spl char and space still this is string
+# mix_peru = 'KGF part-3'
+# print(mix_peru)
+# print(type(mix_peru))
+# #Example for Boolean
+# bo = True # 'T' should be capital
+# print(bo)
+# print(type(bo))
+#
+# nobe = False # 'F' should be capital
+# print(nobe)
+# print(type(nobe))
+# #Example for the spl data type None
+# spl = None #'N' should be capital
+# print(spl)
+# print(type(spl))
+
+"""
+There are few Keyword which we are use them in the python and 
+we can't use those keyword to assign any value
+Example: 'break' is a keyword and we can't use it like 'break = 100' 
+it will throw an error. so if want to know the keywords in python run the below code.
+"""
+
+# import keyword      #first we need to import the keyword in the code.
+# print(keyword.kwlist)   #kwlist is a keyword to get all the list of keyword.
+
